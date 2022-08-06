755b098c0ff771c4f2821c96828f06c5dec897c7
CodingWithLaura <laura.ganss@web.de>
2022-08-06 12:18:49 +0200

    Write a function which interates through list and finds three numbers which add up to 2020 and multiply them

--- day_1/two_entries.py
+++ day_1/two_entries.py
@@ -5,7 +5,17 @@ def two_entries(input):
             if input[x] + input[y] == 2020:
                 result = input[x] * input[y]
     return result
-                
+
+#part two of day_1
+def three_entries(input):
+    result = 0
+    for x in range(0,len(input)):
+        for y in range(1,len(input)-1):
+            for z in range(2,len(input)-2):
+                if input[x] + input[y] + input[z] == 2020:
+                    result = input[x] * input[y] * input[z]
+    return result
+
 
 def main():
     input = [1721,979,366,299,675,1456]
@@ -16,10 +26,12 @@ def main():
 
     for x in lines:
         result.append(int(x.rstrip("\n")))
-    print(result)
     
     test = two_entries(result)
     print(test)
+
+    test2 = three_entries(result)
+    print(test2)
     
 if __name__ == "__main__":
     main()
