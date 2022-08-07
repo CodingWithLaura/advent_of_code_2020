7402bc594b1f51aa4297d008fa66eac66fb3c4be
CodingWithLaura <laura.ganss@web.de>
2022-08-07 16:17:48 +0200

    Format input

--- day_2/passwords.py
+++ day_2/passwords.py
@@ -15,7 +15,58 @@ def passwords(input):
 def main():
  input = [[1,3,"a","abcde"],[1,3,"b","cdefg"],[2,9,"c","ccccccccc"]]
  test = passwords(input)
- print(test)
+ #print(test)
 
+ f = open("numbers_small.txt", "r")
+ lines = f.readlines()
+ result = []
+ bonds =[]
+ letters = []
+ woerter = []
+ tast = []
+
+ for x in lines:
+     y = x.strip("\n")
+     result.append(y.split(" "))
+     
+ for r in result:
+     blubb = r[0].split("-")
+     bonds.append(blubb)
+
+ for b in result:
+     lala =b[1].strip(":")
+     letters.append(lala)
+
+ for c in result:
+     lulu = list(c[2])
+     woerter.append(lulu)
+ print(bonds)
+ print(letters)
+ print(woerter)
+
+ for wort in range(0,len(woerter)):
+  woerter[wort].extend(letters[wort])
+ print(woerter)
+
+ 
+   
+ for wort in woerter:
+  last_element = (wort.pop())
+  count = 0
+  print(last_element)
+  print(wort)
+  for i in wort:
+   if i == last_element:
+    count += 1
+  print(count)
+  for bond in bonds:
+   for y in bond:
+    print(y)
+     
+  
+
+  
+         
+         
 if __name__ == "__main__":
     main()
